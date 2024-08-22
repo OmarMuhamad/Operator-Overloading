c0255993b63b4681bc76c6c0b053c974d131f202
Omar Mohammad <101476960+OmarMuhamad@users.noreply.github.com>
2024-08-23 02:42:20 +0300

    Changed in the main function
    just modified some wrong answers

--- AssignmentI/AssignmentI/Program.cs
+++ AssignmentI/AssignmentI/Program.cs
@@ -19,39 +19,43 @@ namespace AssignmentI
             // Test Case for '+' with integer
             int additionalSeconds = 7800;          // 7800 seconds = 2 hours, 10 minutes
             DateTime d5 = d1 + additionalSeconds;  // 4 hours, 40 minutes, 45 seconds
-            Console.WriteLine("d1 + 7800 seconds = " + d5.ToString()); 
+            Console.WriteLine("d1 + 7800 seconds = " + d5.ToString());
+            DateTime d6 = additionalSeconds + d1;  // 4 hours, 40 minutes, 45 seconds
+            Console.WriteLine("7800 + d1 seconds = " + d6.ToString());
 
             // Test Case for '++'
-            DateTime d6 = new DateTime(1, 59, 59); // 1 hour, 59 minutes, 59 seconds
-            d6++;                                  // 2 hours, 0 minutes, 59 seconds
-            Console.WriteLine("++d6 = " + d6.ToString());     
+            DateTime d7 = new DateTime(1, 59, 59); // 1 hour, 59 minutes, 59 seconds
+            d7++;
+            Console.WriteLine("d7++ = " + (d7).ToString());      // 2 hours, 0 minutes, 59 seconds
+            Console.WriteLine("++d7 = " + (++d7).ToString());    // 2 hours, 1 minutes, 0 seconds
 
             // Test Case for '--'
-            DateTime d7 = new DateTime(1, 1, 0);   // 1 hour, 0 minutes, 0 seconds
-            d7--;                                  // 0 hours, 59 minutes, 0 seconds
-            Console.WriteLine("--d7 = " + d7.ToString());     
+            DateTime d8 = new DateTime(1, 1, 0);   // 1 hour, 1 minutes, 0 seconds
+            d8--;
+            Console.WriteLine("d8-- = " + d8.ToString()); // 1 hours, 0 minutes, 0 seconds
+            Console.WriteLine("--d8 = " + (--d8).ToString()); // 0 hours, 59 minutes, 0 seconds
 
             // Test Case for '>'
-            Console.WriteLine("d1 > d2 = " + (d1 < d2)); 
+            Console.WriteLine("d1 > d2 = " + (d1 > d2)); 
 
             // Test Case for '<'
-            Console.WriteLine("d2 < d1 = " + (d2 > d1)); 
+            Console.WriteLine("d1 < d2 = " + (d1 < d2)); 
 
             // Test Case for '<='
             Console.WriteLine("d2 <= d1 = " + (d2 <= d1)); 
 
             // Test Case for '>='
-            Console.WriteLine("d1 >= d2 = " + (d1 >= d2)); 
+            Console.WriteLine("d2 >= d1 = " + (d2 >= d1)); 
 
             // Test Case for '=='
-            DateTime d8 = new DateTime(1, 20, 30);
-            Console.WriteLine("d2 == d8 = " + (d2 == d8)); 
+            DateTime d9 = new DateTime(1, 20, 30);
+            Console.WriteLine("d2 == d9 = " + (d2 == d9)); 
 
             // Test Case for '!='
             Console.WriteLine("d1 != d2 = " + (d1 != d2));
 
-            if (d8) Console.WriteLine("True");
-            else Console.WriteLine("False");
+            if (d9) Console.WriteLine("d9 is not null");
+            else Console.WriteLine("d9 is null");
         }
     
     }
