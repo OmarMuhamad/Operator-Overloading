8fa83e08ccd7d0a1f5c23080baa18693396226d6
Omar Mohammed <101476960+OmarMuhamad@users.noreply.github.com>
2024-08-22 06:58:17 +0300

    All Files uploaded

---
+++ AssignmentI/AssignmentI/DateTime.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentI
+{
+    internal class DateTime
+    {
+        public int Hours {  get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        
+        public DateTime(int hours = 0, int minutes = 0, int seconds = 0)
+        {
+            this.Hours = hours;
+            this.Minutes = minutes;
+            this.Seconds = seconds;
+        }
+        public static DateTime operator -(DateTime left, DateTime right)
+        {
+            DateTime newDateTime = new DateTime();
+            int totalLSeconds = left.Hours * 3600 + left.Minutes * 60 + left.Seconds;
+            int totalRSeconds = right.Hours * 3600 + right.Minutes * 60 + right.Seconds;
+            int resultSeconds = totalLSeconds - totalRSeconds;
+            if (resultSeconds < 0) { 
+                return new DateTime(); 
+            }
+            newDateTime.Hours = resultSeconds / 3600;
+            resultSeconds -= newDateTime.Hours * 60 * 60;
+            newDateTime.Minutes = resultSeconds / 60;
+            resultSeconds -= newDateTime.Minutes * 60;
+            newDateTime.Seconds = resultSeconds;
+            return newDateTime;
+        }
+        public static DateTime operator +(DateTime left, DateTime right)
+        {
+            DateTime newDateTime = new DateTime();
+            newDateTime.Seconds = (left.Seconds + right.Seconds) % 60;
+            newDateTime.Minutes = (left.Minutes + right.Minutes) % 60 + (left.Seconds + right.Seconds) / 60;
+            newDateTime.Hours = (left.Hours + right.Hours) + (left.Minutes + right.Minutes) / 60;
+            return newDateTime;
+        }
+        public static DateTime operator +(DateTime DateTime, int secs)
+        {
+            DateTime newDateTime = new DateTime();
+            newDateTime.Hours = secs / 3600;
+            secs -= newDateTime.Hours * 60 * 60;
+            newDateTime.Minutes = secs / 60;
+            secs -= newDateTime.Minutes * 60;
+            newDateTime.Seconds = secs;
+
+            return newDateTime + DateTime;
+        }
+        public static DateTime operator +(int secs, DateTime DateTime)
+        {
+            DateTime newDateTime = new DateTime();
+            newDateTime.Hours = secs / 3600;
+            secs -= newDateTime.Hours * 60 * 60;
+            newDateTime.Minutes = secs / 60;
+            secs -= newDateTime.Minutes * 60;
+            newDateTime.Seconds = secs;
+
+            return newDateTime + DateTime;
+        }
+        public static DateTime operator ++(DateTime DateTime)
+        {
+            DateTime.Minutes++;
+            DateTime.Hours += DateTime.Minutes / 60;
+            DateTime.Minutes %= 60;
+            return DateTime;
+        }
+        public static DateTime operator --(DateTime DateTime)
+        {
+            DateTime.Minutes--;
+            if (DateTime.Minutes < 0)
+            {
+                if (DateTime.Hours > 0)
+                {
+                    DateTime.Minutes = 59;
+                    DateTime.Hours--;
+                }
+                else DateTime.Minutes = 0;
+            }
+            return DateTime;
+        }
+        public static bool operator >(DateTime left, DateTime right)
+        {
+            if (left.Hours > right.Hours)
+            {
+                return true;
+            }
+            else if (left.Hours == right.Hours)
+            {
+                if (left.Minutes > right.Minutes)
+                {
+                    return true;
+                }
+                else if (left.Minutes == right.Minutes)
+                {
+                    return left.Seconds > right.Seconds;
+                }
+            }
+            return false;
+        }
+        public static bool operator <(DateTime left, DateTime right)
+        {
+            if (left.Hours < right.Hours)
+            {
+                return true;
+            }
+            else if (left.Hours == right.Hours)
+            {
+                if (left.Minutes < right.Minutes)
+                {
+                    return true;
+                }
+                else if (left.Minutes == right.Minutes)
+                {
+                    return left.Seconds < right.Seconds;
+                }
+            }
+            return false;
+        }
+        public static bool operator <=(DateTime left, DateTime right)
+        {
+            if (left.Hours < right.Hours)
+            {
+                return true;
+            }
+            else if (left.Hours == right.Hours)
+            {
+                if (left.Minutes < right.Minutes)
+                {
+                    return true;
+                }
+                else if (left.Minutes == right.Minutes)
+                {
+                    return left.Seconds <= right.Seconds;
+                }
+            }
+            return false;
+        }
+        public static bool operator >=(DateTime left, DateTime right)
+        {
+            if (left.Hours > right.Hours)
+            {
+                return true;
+            }
+            else if (left.Hours == right.Hours)
+            {
+                if (left.Minutes > right.Minutes)
+                {
+                    return true;
+                }
+                else if (left.Minutes == right.Minutes)
+                {
+                    return left.Seconds >= right.Seconds;
+                }
+            }
+            return false;
+        }
+        public static bool operator ==(DateTime left, DateTime right)
+        {
+            if (left.Hours == right.Hours && left.Minutes == right.Minutes && left.Seconds == right.Seconds)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(DateTime left, DateTime right)
+        {
+            if (left.Hours == right.Hours && left.Minutes == right.Minutes && left.Seconds == right.Seconds)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool operator true(DateTime DateTime)
+        {
+            return DateTime.Hours > 0 || DateTime.Minutes > 0 || DateTime.Seconds > 0;
+        }
+        public static bool operator false(DateTime DateTime)
+        {
+            return DateTime.Hours == 0 && DateTime.Minutes == 0 && DateTime.Seconds == 0;
+        }
+        public override string ToString()
+        {
+            return $"{Hours:D2}:{Minutes:D2}:{Seconds:D2}";
+        }
+    }
+}
---
+++ AssignmentI/AssignmentI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AssignmentI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test Case for '-'
+            DateTime d1 = new DateTime(2, 30, 45); // 2 hours, 30 minutes, 45 seconds
+            DateTime d2 = new DateTime(1, 20, 30); // 1 hour, 20 minutes, 30 seconds
+            DateTime d3 = d1 - d2;                 // 1 hour, 10 minutes, 15 seconds
+            Console.WriteLine("d1 - d2 = " + d3.ToString());  
+
+            // Test Case for '+'
+            DateTime d4 = d1 + d2;                 // 3 hours, 51 minutes, 15 seconds
+            Console.WriteLine("d1 + d2 = " + d4.ToString());  
+
+            // Test Case for '+' with integer
+            int additionalSeconds = 7800;          // 7800 seconds = 2 hours, 10 minutes
+            DateTime d5 = d1 + additionalSeconds;  // 4 hours, 40 minutes, 45 seconds
+            Console.WriteLine("d1 + 7800 seconds = " + d5.ToString()); 
+
+            // Test Case for '++'
+            DateTime d6 = new DateTime(1, 59, 59); // 1 hour, 59 minutes, 59 seconds
+            d6++;                                  // 2 hours, 0 minutes, 59 seconds
+            Console.WriteLine("++d6 = " + d6.ToString());     
+
+            // Test Case for '--'
+            DateTime d7 = new DateTime(1, 1, 0);   // 1 hour, 0 minutes, 0 seconds
+            d7--;                                  // 0 hours, 59 minutes, 0 seconds
+            Console.WriteLine("--d7 = " + d7.ToString());     
+
+            // Test Case for '>'
+            Console.WriteLine("d1 > d2 = " + (d1 < d2)); 
+
+            // Test Case for '<'
+            Console.WriteLine("d2 < d1 = " + (d2 > d1)); 
+
+            // Test Case for '<='
+            Console.WriteLine("d2 <= d1 = " + (d2 <= d1)); 
+
+            // Test Case for '>='
+            Console.WriteLine("d1 >= d2 = " + (d1 >= d2)); 
+
+            // Test Case for '=='
+            DateTime d8 = new DateTime(1, 20, 30);
+            Console.WriteLine("d2 == d8 = " + (d2 == d8)); 
+
+            // Test Case for '!='
+            Console.WriteLine("d1 != d2 = " + (d1 != d2));
+
+            if (d8) Console.WriteLine("True");
+            else Console.WriteLine("False");
+        }
+    
+    }
+}
